1a927a40a9442cd6b9ac5c7b39c31d7fc26d3395
Suppakron <suwannapop01@gmail.com>
2023-07-24 22:30:32 +0700

    NOO

--- src/app/page.js
+++ src/app/page.js
@@ -8,75 +8,84 @@ import { nanoid } from "nanoid";
 import { useState } from "react";
 
 export default function Home() {
-  //tasks = array of {id: string, title: string, completed: boolean}
-  const [tasks, setTasks] = useState([]);
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
+  //tasks = array of {id: string, title: string, completed: boolean}
+  const [tasks, setTasks] = useState([]);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-  const addTask = (newTaskTitle) => {
-    const newTask = { id: nanoid(), title: newTaskTitle, completed: false };
-    const newTasks = [...tasks, newTask];
-    setCount1(count1 + 1);
-    setTasks(newTasks);
-  };
+  const addTask = (newTaskTitle) => {
+    const newTask = { id: nanoid(), title: newTaskTitle, completed: false };
+    const newTasks = [...tasks, newTask];
+    setCount1(count1 + 1);
+    setTasks(newTasks);
+  };
 
-  const deleteTask = (taskId) => {
-    const newTasks = tasks.filter((task) => task.id !== taskId);
-    setCount1(count1 - 1);
-    setTasks(newTasks);
+  const deleteTask = (taskId) => {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setCount1(count1 - 1);
+    setTasks(newTasks);
 
-    const taskToDelete = tasks.find((task) => task.id === taskId);
-    if (taskToDelete && taskToDelete.completed) {
-      setCount2(Math.max(count2 - 1, 0));
-    }
-  };
+    const taskToDelete = tasks.find((task) => task.id === taskId);
+    if (taskToDelete && taskToDelete.completed) {
+      setCount2(Math.max(count2 - 1, 0));
+    }
+  };
 
-  const toggleDoneTask = (taskId) => {
-    //structuredClone will copy an array or an object "deeply"
-    //So objects within an object will be copied too
-    const newTasks = structuredClone(tasks);
-    //search for a task based on condition
-    const task = newTasks.find((x) => x.id === taskId);
-    const isTaskCompleted = task.completed;
-    task.completed = !task.completed;
-    setTasks(newTasks);
+  const toggleDoneTask = (taskId) => {
+    //structuredClone will copy an array or an object "deeply"
+    //So objects within an object will be copied too
+    const newTasks = structuredClone(tasks); //search for a task based on condition
+    const task = newTasks.find((x) => x.id === taskId);
+    const isTaskCompleted = task.completed;
+    task.completed = !task.completed;
+    setTasks(newTasks);
 
-    if (task.completed && !isTaskCompleted) {
-      setCount2(count2 + 1);
-    } else if (!task.completed && isTaskCompleted) {
-      setCount2(Math.max(count2 - 1, 0));
-    }
-  };
+    if (task.completed && !isTaskCompleted) {
+      setCount2(count2 + 1);
+    } else if (!task.completed && isTaskCompleted) {
+      setCount2(Math.max(count2 - 1, 0));
+    }
+  };
 
-  return (
-    // Main container
-    <div className="container mx-auto">
-      {/* header section */}
-      <Header />
-      {/* tasks container */}
-      <div style={{ maxWidth: "400px" }} className="mx-auto">
-        {/* Task summary */}
-        <p className="text-center text-secondary fst-italic">
-          All ({count1}) Done ({count2})
-        </p>
-        {/* task input */}
-        <TaskInput addTaskFunc={addTask} />
-
-        {/* tasks mapping*/}
-        {tasks.map((task) => (
-          <Task
-            id={task.id}
-            title={task.title}
-            deleteTaskFunc={deleteTask}
-            toggleDoneTaskFunc={toggleDoneTask}
-            completed={task.completed}
-            key={task.id}
-          />
-        ))}
-      </div>
-
-      {/* //footer section */}
-      <Footer year="2023" fullName="Pongporn Seetong" studentId="650612091" />
-    </div>
-  );
+  return (
+    // Main container
+    <div className="container mx-auto">
+            {/* header section */}
+            
+      <Header />
+            {/* tasks container */}
+            
+      <div style={{ maxWidth: "400px" }} className="mx-auto">
+                {/* Task summary */}
+                
+        <p className="text-center text-secondary fst-italic">
+                    All ({count1}) Done ({count2})         
+        </p>
+                {/* task input */}
+                
+        <TaskInput addTaskFunc={addTask} />
+                {/* tasks mapping*/}
+                
+        {tasks.map((task) => (
+          <Task
+            id={task.id}
+            title={task.title}
+            deleteTaskFunc={deleteTask}
+            toggleDoneTaskFunc={toggleDoneTask}
+            completed={task.completed}
+            key={task.id}
+          />
+        ))}
+              
+      </div>
+            {/* //footer section */}
+            
+      <Footer
+        year="2023"
+        fullName="Suppakorn Suwannapop"
+        studentId="650612100"
+      />
+          
+    </div>
+  );
 }
